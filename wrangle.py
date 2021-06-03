2a1955a5a385b1cd81d41a21dc67f898494a1fc8
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-06-03 15:01:50 -0500

    update wrangle.py- add split

--- wrangle.py
+++ wrangle.py
@@ -7,7 +7,7 @@ import os
 from pydataset import data
 
 # acquire
-from env import host, user, password
+import acquire
 
 
 # Create helper function to get the necessary connection url.
@@ -35,15 +35,36 @@ def get_telco_data():
     '''
     return pd.read_sql(sql_query, get_connection('telco_churn'))
 
+############################ Split Data Function ##############################
 
+def split_data(df):
+    '''
+    split our data,
+    takes in a pandas dataframe
+    returns: three pandas dataframes, train, test, and validate
+    '''
+    train_val, test = train_test_split(df, train_size=0.8, random_state=123)
+    train, validate = train_test_split(train_val, train_size=0.7, random_state=123)
+    return train, validate, test
 
 ############################ Wrangle Function ##############################
 
 
-def wrangle_telco(df):
+def wrangle_telco():
+'''
+This function checks to see if telco_churn.csv already exists, 
+if it does not, one is created
+then the data is cleaned and the dataframe is returned
+'''
+    #check to see if telco_churn.csv already exist
+    if.os.path.isfile(telco_churn.csv):
+        df = pd.read_csv('telco_churn.csv', index_col=0)
+    
+    else:
 
-    #acquire data
-    df = get_telco_data(df)
+        #creates new csv if one does not already exist
+        df = new_telco_data()
+        df.to_csv('telco_churn.csv')
 
     #replace blank spaces and special characters
     df = df.replace(r'^\s*$', np.nan, regex=True)
@@ -51,8 +72,8 @@ def wrangle_telco(df):
     #change total_charges to float from object
     df.total_charges = df.total_charges.str.replace(' ', '0').astype(float)
 
-    #drop NaN values (10 out of 1695 isnt significant)
-    df = df.dropna()
+    #fill NaN values (10 out of 1695) with monthly charges
+    df.total_charges = df.total_charges.fillna(df.monthly_charges)
 
     return df
 

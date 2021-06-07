9639485e88a553f54214ba8bfb00651b419e124c
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-06-07 09:39:25 -0500

    add grades_wrangle for lesson plan

--- wrangle.py
+++ wrangle.py
@@ -129,3 +129,25 @@ def split_data(df):
     print(f'validate -> {validate.shape}')
     print(f'test -> {test.shape}')
     return train, validate, test
+
+
+def wrangle_grades():
+    '''
+    Read student_grades csv file into a pandas DataFrame,
+    drop student_id column, replace whitespaces with NaN values,
+    drop any rows with Null values, convert all columns to int64,
+    return cleaned student grades DataFrame.
+    '''
+    # Acquire data from csv file.
+    grades = pd.read_csv('student_grades.csv')
+    
+    # Replace white space values with NaN values.
+    grades = grades.replace(r'^\s*$', np.nan, regex=True)
+    
+    # Drop all rows with NaN values.
+    df = grades.dropna()
+    
+    # Convert all columns to int64 data types.
+    df = df.astype('int')
+    
+    return df

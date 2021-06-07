9e54c28a797bc74f3e40e30a3501ca99f39ede16
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-06-07 15:14:18 -0500

    change code to be reproducible

--- explore.py
+++ explore.py
@@ -1,3 +1,18 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from scipy.stats import pearsonr, spearmanr
+
+import env
+import wrangle
+
 ########################## Tenure Function ##########################
 
 def months_to_years(df):
@@ -11,9 +26,24 @@ def months_to_years(df):
 
 ################### Plot Variable Pairs Function ##################
 
-def plot_variable_pairs(df):
-    columns = df[list(df.select_dtypes(exclude = 'O').columns)]
+def plot_variable_pairs(df, target):
+    columns = df[list(df.select_dtypes(exclude = 'O').columns)].drop(columns=target)
     sns.set(style='whitegrid', palette='muted')
     g = sns.PairGrid(columns)
     g = g.map(sns.regplot, line_kws={'color':'red'})
+
+
+################### Plot Cat&Cont Function ##################
+def plot_categorical_and_continuous_vars(df, categorical_var, continuous_var):
+    '''
+    this function that accepts your dataframe 
+    and the name of the columns that hold the continuous and categorical features 
+    outputs: 3 different plots for visualizing a categorical variable and a continuous variable.
+    '''
+    sns.swarmplot(data=df, x=categorical_var, y=continuous_var)
+    plt.show()
+    sns.boxplot(data=df, x=categorical_var, y=continuous_var)
+    plt.show()
+    sns.barplot(data=df, x=categorical_var, y=continuous_var)
+    plt.show()
 

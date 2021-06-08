d7eca5f6261fb0993b134ce3852f3944832dad3f
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-06-08 16:03:12 -0500

    create function for better_than_baseline

--- evaluate.py
+++ evaluate.py
@@ -22,34 +22,30 @@ def plot_residuals(df, y, yhat):
     This function takes in actual value and predicted value 
     then creates a scatter plot
     '''
-   #baseline
-    df['yhat_baseline'] = df[y].mean()
-    
     # residuals
     df['residuals'] = df[y] - df[yhat]
-    df['baseline_residuals'] = df[y] - df['yhat_baseline']
     
     fig, ax = plt.subplots(figsize=(13, 7))
-    ax.hist(df.baseline_residuals, label='baseline residuals', alpha=.6)
     ax.hist(df.residuals, label='model residuals', alpha=.6)
     ax.legend()
     return
     
 #####################FUNCTION FOR REGRESSION ERRORS#####################
 
-def regression_errors(y, yhat):
+def regression_errors(df, y, yhat):
     '''
     This function takes in actual value and predicted value 
     then outputs: the sse, ess, tss, mse, and rmse
     '''
-    residuals= y- yhat
+    df['residuals']= df[y]- df[yhat]
     sse = (df.residuals **2).sum()
-    ess= ((df.yhat - df.y.mean()) **2).sum()
-    tss = ((df.y- df.y.mean())**2).sum()
+    ess= ((df[yhat] - df[y].mean()) **2).sum()
+    tss = ((df[y]- df[y].mean())**2).sum()
     n = df.shape[0]
     mse = sse/n
     rmse = math.sqrt(mse)
-        print(f''' 
+
+    print(f''' 
         sse: {sse: .4f}
         ess: {ess: .4f}
         tss: {tss: .4f}
@@ -59,17 +55,18 @@ def regression_errors(y, yhat):
 
 #####################FUNCTION FOR BASELINE ERRORS#####################
 
-def baseline_mean_errors(y):
+def baseline_mean_errors(df, y, yhat):
     '''
     This function takes in actual value and predicted value
     then outputs: the SSE, MSE, and RMSE for the baseline model
     '''
-    residuals= y- yhat
-    sse_baseline = (df.residuals **2).sum()
+    df['residuals_baseline']= df[y]- df[yhat]
+    sse_baseline = (df.residuals_baseline **2).sum()
     n = df.shape[0]
-    mse_baseline = sse/n
+    mse_baseline = sse_baseline/n
     rmse_baseline = math.sqrt(mse_baseline)
-        print(f''' 
+
+    print(f''' 
         sse_baseline: {sse_baseline: .4f}
         mse_baseline: {mse_baseline: .4f}
         rmse_baseline: {rmse_baseline: .4f}
@@ -77,11 +74,25 @@ def baseline_mean_errors(y):
 
 ##################FUNCTION TO RETURN BETTER THAN BASELINE##################
 
-def better_than_baseline(y, yhat):
+def better_than_baseline(df, y, yhat):
     '''
     This function takes in actual value and predicted value
     then returns true if your model performs better than the baseline, otherwise false
     '''
+    #calculate the rmse 
+    df['residuals']= df[y]- df[yhat]
+    sse = (df.residuals **2).sum()
+    n = df.shape[0]
+    mse = sse/n
+    rmse = math.sqrt(mse)
+
+    #calculate the rmse baseline
+    df['residuals_baseline']= df[y]- df[yhat]
+    sse_baseline = (df.residuals_baseline **2).sum()
+    n = df.shape[0]
+    mse_baseline = sse_baseline/n
+    rmse_baseline = math.sqrt(mse_baseline)
+
     if rmse < rmse_baseline:
         return True
     else:
